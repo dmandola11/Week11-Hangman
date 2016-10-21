c4da9fad0a2a270d89c78e9836489742eb17b7d8
Damian Mandola Jr <damianjr@Damian-Mandola-Jrs-MacBook-Pro.local>
2016-10-21 13:38:01 -0500

    created wordbank in game.js

---
+++ game.js
@@ -0,0 +1,5 @@
+function WordBank() {
+	this.wordBank = ['cappuccino', 'latte', 'americano', 'macchiato', 'espresso', 'cortado', 'doppio', 'ristretto', 'lungo', 'breve'];
+}
+
+module.exports = WordBank;
